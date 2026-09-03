09614cc7f0edfe21c9bca390f279e37e2eb74c1b
Sanjib Narzary <o-._.-o@live.com>
2026-09-03 12:12:19 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/kimi-k3-asic/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
